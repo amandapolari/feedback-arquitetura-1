0a576de37c890c7f8fd1dce507328b4055bea106
FernandaImai <fernanda.imai@gmail.com>
2023-11-07 20:49:16 -0300

    controller-delete

--- src/controller/CryptocoinController.ts
+++ src/controller/CryptocoinController.ts
@@ -1,3 +1,5 @@
+import { CryptocoinBusiness } from "../business/CryptocoinBusiness"
+
 export class CryptocoinController {
   public getAllCoins = async () => {
 
@@ -11,7 +13,49 @@ export class CryptocoinController {
 
   }
 
-  public deleteCoin = async () => {
+  public deleteCoin = async (req:Request , res:Response) =>{
+      try {
+          const input = {id: req.params.id}
+          
+          const cryptocoinBusiness = new CryptocoinBusiness()
 
+          const response = await cryptocoinBusiness.deleteCoin(input)
+  
+          // if (typeof idToDelete !== "string") {
+          //     res.status(400)
+          //     throw new Error("'id' deve ser string")
+          // }
+  
+          // const userDataBase = new UsersDatabase();
+          // const userDBExists = await userDataBase.findUserById(idToDelete);
+  
+          // if (!userDBExists) {
+          //     res.status(404)
+          //     throw new Error("'id' não encontrado")
+          // }
+  
+          // const user = new User(
+          //     userDBExists.id,
+          //     userDBExists.name,
+          //     userDBExists.created_at
+          // )
+  
+          // await userDataBase.deleteUserById(user.getId());
+          
+          // res.status(200).send("Usuario deletado com sucesso")
+      } catch (error) {
+          console.log(error)
+  
+          if (req.statusCode === 200) {
+              res.status(500)
+          }
+  
+          if (error instanceof Error) {
+              res.send(error.message)
+          } else {
+              res.send("Erro inesperado")
+          }
+      }
+    }
   }
-}
+
